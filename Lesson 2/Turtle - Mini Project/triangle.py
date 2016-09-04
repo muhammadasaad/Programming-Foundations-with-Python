f816dab412ed2dff5ce37fbe245268ab6f55d54f
muhammadasaad <asaad.fayyaz@gmail.com>
2016-09-04 10:46:22 +0500

    Frectal by Thai Thien.py
    only improved the code so that no redundant passes are made.

--- Lesson 2/Turtle - Mini Project/triangle.py
+++ Lesson 2/Turtle - Mini Project/triangle.py
@@ -1,37 +1,46 @@
-import turtle
-#Haha TTpro
-def draw_triangle(the_turtle,length,ori,recursion):
-    recursion=recursion+1
-    meow= the_turtle
+#minor changes to thai thien code
 
-    for i in range(0,3):
-        if(recursion<4):
-        #if (0):
-            meow.forward(length/2)
-            meow.left(120)
-            draw_triangle(meow,length/2,0,recursion)
-            meow.right(120)
-            meow.forward(length/2)
-        else:
+import turtle
+
+recursion_depth = 4 #depth of the frectal
+triangle_size = 200 #outer triangle size
+
+def draw_triangle(meow,length,recursion):
+    recursion=recursion+1
+    if (recursion == 1):
+        meow.speed(1)
+        meow.forward(length/2)
+        meow.left(120)
+        for i in range(2):
             meow.forward(length)
-        if (ori==1):
             meow.left(120)
-        else:
+        meow.forward(length/2)
+        meow.left(120)
+        meow.speed(10)
+        draw_triangle(meow,length/2,recursion)
+        
+    else:
+        for i in range(0,3):
+            if(recursion<recursion_depth):
+                meow.forward(length/2)
+                meow.left(120)
+                draw_triangle(meow,length/2,recursion)
+                meow.right(120)
+                meow.forward(length/2)
+            else:
+                meow.forward(length)
             meow.right(120)
+
 
 meow = turtle.Turtle() # init
-meow.speed(10) # speed = 1 to slow turtle down
-meow.color("yellow","green") # set color5
+meow.color("blue","green") # set color5
 meow.shape("turtle") # set shape to a turtle
+meow.width(2) #line thickness
 background = turtle.Screen()  # create background
-background.bgcolor("red")     # set background color to red
+background.bgcolor("white")     # set background color to red
 
-
-draw_triangle(meow,200,1,0)
-
-#meow.forward(100)
-#meow.left(120)
-#draw_triangle(meow,100,0,0)
-#meow.right(120)
+meow.fill(True) #fill the drawing
+draw_triangle(meow,triangle_size,0)
+meow.fill(False)
 
 background.exitonclick()      # click anywhere to close background
